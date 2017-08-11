87ebe256594c00bd1e9ae0e68708b69977851826
ChandrasekharReddy <tchandu.soft@gmail.com>
2017-08-11 12:44:52 +0530

    Update ngx-datatable.ts

--- app/ngx-datatable.ts
+++ app/ngx-datatable.ts
@@ -1,36 +1,6 @@
- rows = [
-    { policyno: 'PC1', policyholdername: 'Chandra', policytype: 'Auto', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
-    { policyno: 'PC2', policyholdername: 'Sai', policytype: 'Health', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
-    { policyno: 'PC3', policyholdername: 'Anitha', policytype: 'Fire', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
-    { policyno: 'PC4', policyholdername: 'Chandra', policytype: 'Auto', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
-    { policyno: 'PC5', policyholdername: 'Sai', policytype: 'Health', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
-    { policyno: 'PC6', policyholdername: 'Anitha', policytype: 'Fire', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
-  ];
-  columns = [
-    // { prop: 'name' },
-    { prop: 'policyno'},
-    { prop: 'policyholdername' },
-    { prop: 'policytype' },
-    { prop: 'Company1' },
-    { prop: 'Company2' },
-    { prop: 'Company3' },
-    { prop: 'Company4' },
-  ];
-  selected = [];
-  onSelect({ selected }) {
-    console.log('Select Event', selected, this.selected);
-
-    this.selected.splice(0, this.selected.length);
-    this.selected.push(...selected);
-  }
-
-  onActivate(event) {
-    console.log('Activate Event', event);
-  }
-  
-  <div style='float:left;width:100%'>
+<div style='width:100%'>
         <ngx-datatable
-          style="width: 90%"
+          style="width: 100%"
           class="material"
           [rows]="rows"
           [columnMode]="'force'"
@@ -51,12 +21,56 @@
             [headerCheckboxable]="true"
             [checkboxable]="true">
           </ngx-datatable-column>
-          <ngx-datatable-column name="Policy No" prop="policyno"></ngx-datatable-column>
-          <ngx-datatable-column name="Policy Holder Name" prop="policyholdername"></ngx-datatable-column>
-          <ngx-datatable-column name="Policy Type" prop="policytype"></ngx-datatable-column>
-          <ngx-datatable-column name="Company1"></ngx-datatable-column>
-          <ngx-datatable-column name="Company2"></ngx-datatable-column>
-          <ngx-datatable-column name="Company3"></ngx-datatable-column>
-          <ngx-datatable-column name="Company4"></ngx-datatable-column>
+
+          <div *ngFor="let clmn of columns">   
+            <ngx-datatable-column name="{{clmn.prop}}" prop="{{clmn.prop}}"></ngx-datatable-column>
+          </div>
+         
         </ngx-datatable>
       </div>
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'table-component',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  tableColumns: any[] = [];
+  columns: any[] = [];
+  column: any = {};
+  rows = [
+    { policyno: 'PC1', policyholdername: 'Chandra', policytype: 'Auto', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
+    { policyno: 'PC2', policyholdername: 'Sai', policytype: 'Health', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
+    { policyno: 'PC3', policyholdername: 'Anitha', policytype: 'Fire', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
+    { policyno: 'PC4', policyholdername: 'Chandra', policytype: 'Auto', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
+    { policyno: 'PC5', policyholdername: 'Sai', policytype: 'Health', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
+    { policyno: 'PC6', policyholdername: 'Anitha', policytype: 'Fire', company1: 'Swimlane', company2: 'Swimlane', company3: 'Swimlane', company4: 'Swimlane' },
+  ];
+
+  ngOnInit() {
+    this.tableColumns = Object.keys(this.rows[0]);
+    this.tableColumns.forEach(element => {
+      this.columns.push({
+        prop: element
+      });
+    });
+  }
+
+  selected = [];
+  onSelect({ selected }) {
+    // console.log('Select Event', selected, this.selected);
+
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+    console.log(this.selected);
+  }
+
+  onActivate(event) {
+    console.log('Activate Event', event);
+  }
+}
