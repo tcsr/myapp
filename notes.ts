2d9e042b0e198d0218cb64726efaf6683c5e6260
ChandrasekharReddy <tchandu.soft@gmail.com>
2017-09-12 11:23:25 +0530

    Create notes.ts

---
+++ notes.ts
@@ -0,0 +1,30 @@
+mainPD:any = {};
+  pd: any =[];
+  panelData = {};
+
+  panelArray: any = [];
+  panelKeys: any;
+  filteredPanelArray: any = [];
+  matchedPanel: any = {};
+
+
+this.dynamicData['dynamicFields'].PANEL_CONFIG.forEach(element => {
+      this.panelKeys = Object.keys(element);
+      console.log(this.panelKeys);
+    });
+
+
+    this.dynamicData["dynamicFields"].PANEL_DETAILS.forEach(fields => {
+
+      if (this.panelKeys.includes(fields.SCR_LOC)) {   //this.panelKeys.indexOf(fields.SCR_LOC) > -1
+        let panel = fields.SCR_LOC;
+        
+        let panels = this.dynamicData['dynamicFields'].PANEL_CONFIG[0][panel];
+        this.pd.push(panels);
+        // this.matchedPanel[panelElement] = fields;
+        this.panelArray.push(fields);
+        // console.log(this.matchedPanel);
+      }
+
+    });
+    console.log(this.pd);
